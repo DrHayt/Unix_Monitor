52e87f5255bc1d1d8fb0111639ec2b94723a0aa7
Rick Moran <drhayt@gmail.com>
2015-02-21 22:20:59 -0500

    Another one down

---
+++ SGMonitor/SPIDataService_FileAccessibility.pm
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+package SGMonitor::SPIDataService_FileAccessibility;
+use SGMonitor::Helpers::ServiceBus;
+use Data::Dumper;
+use Net::Statsd;
+use Sys::Hostname;
+
+sub new(){
+    my ($class,$args)=@_;
+    my $self = {};
+
+    $self->{SB}=SGMonitor::Helpers::ServiceBus->new( $args );
+    $self->{host}=hostname();
+    $self->{host}=~ s/\./_/g;
+
+    $self->{BASE_STRING}="ServiceBus.monitor.SPIDATASERVICE.FileAccessibility";
+
+    return(bless($self,$class));
+}
+
+sub run(){
+    my $self=shift;
+    my $start=100100100;
+    my $range=30000000;
+    my $ordernumber=int(rand($range))+$start;
+
+    my %params=( 'ordernumber' => $ordernumber );
+
+    my ($elapsed,$status,$extra)=$self->{SB}->call_object("SPIDataService.FileAccessibility",\%params);
+
+    Net::Statsd::timing($self->{BASE_STRING}.".".$status,$elapsed*1000);
+
+    #if( $status eq "SUCCESS"){
+    #    print("$status: Order #$ordernumber took $elapsed seconds\n");
+    #} else {
+    #    print("$status: Order #$ordernumber took $elapsed seconds with body ".Dumper($extra)."\n");
+    #}
+}
+
+
+1;
