dcc087688933592f34f00891dcb9fdfda51b52b9
Rick Moran <drhayt@gmail.com>
2015-02-21 19:36:11 -0500

    Reporting values appropriately

--- SGMonitor/ContentService_List.pm
+++ SGMonitor/ContentService_List.pm
@@ -3,12 +3,18 @@ use warnings;
 package SGMonitor::ContentService_List;
 use SGMonitor::Helpers::ServiceBus;
 use Data::Dumper;
+use Net::Statsd;
+use Sys::Hostname;
 
 sub new(){
     my ($class,$args)=@_;
     my $self = {};
 
     $self->{SB}=SGMonitor::Helpers::ServiceBus->new( $args );
+    $self->{host}=hostname();
+    $self->{host}=~ s/\./_/g;
+
+    $self->{BASE_STRING}="ServiceBus.monitor.CONTENTSERVICE.LIST";
 
     return(bless($self,$class));
 }
@@ -22,11 +28,14 @@ sub run(){
     my %params=( 'orderId' => $ordernumber );
 
     my ($elapsed,$status,$extra)=$self->{SB}->call_object("ContentService.List",\%params);
-    if( $status eq "SUCCESS"){
-        print("$status: Order #$ordernumber took $elapsed seconds\n");
-    } else {
-        print("$status: Order #$ordernumber took $elapsed seconds with body $extra\n");
-    }
+
+    Net::Statsd::timing($self->{BASE_STRING}.".".$status,$elapsed*1000);
+
+    #if( $status eq "SUCCESS"){
+        #print("$status: Order #$ordernumber took $elapsed seconds\n");
+    #} else {
+        #print("$status: Order #$ordernumber took $elapsed seconds with body $extra\n");
+    #}
 }
 
 
