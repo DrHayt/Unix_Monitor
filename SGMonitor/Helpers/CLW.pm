54a711ab55300e2dc6aac604949798e3fab20347
Rick Moran <drhayt@gmail.com>
2015-02-23 11:42:03 -0500

    Added CLW helper

---
+++ SGMonitor/Helpers/CLW.pm
@@ -0,0 +1,142 @@
+use strict;
+use warnings;
+package SGMonitor::Helpers::ServiceBus;
+use Data::Dumper;
+use LWP::UserAgent;
+use Time::HiRes qw(gettimeofday);
+use JSON;
+
+sub new(){
+    my ($class,$args)=@_;
+    my $self = {};
+
+    $self->{DEBUG} = $args->{DEBUG} || 0;
+    $self->{URL}=$args->{url} || 'https://clw.safeguardproperties.com/cgi-bin/websvc.cgi';
+    # https://clw.safeguardproperties.com/cgi-bin/websvc.cgi?app=WD180&PropertyNumber=7233738
+    $self->{TIMEOUT}=$args->{timeout} || 10;
+
+    $self->{UA}= LWP::UserAgent->new();
+    $self->{UA}->timeout($self->{TIMEOUT});
+
+    $self->{ENCODER}=JSON->new->allow_nonref;
+    $self->{ENCODER}->allow_blessed;
+
+    return(bless($self,$class));
+}
+
+
+sub call_object($\%){
+        my $self=shift;
+        my $method=shift;
+        my $params_obj=shift;
+
+
+        my @params;
+        my $encoded_params;
+
+        foreach my $key(%{$params_obj}){
+            push(@params,"$key"."=".$params_obj->{$key});
+        }
+
+        my $encoded_params=join('&',@params);
+
+        if ($self->{DEBUG}){
+            $self->trace_log($id,
+                            'call_object',
+                            scalar(caller),
+                            "ARGS:  method:\"$method\", params_obj: ".$self->{ENCODER}->encode($params_obj)
+                            );
+        }
+
+        my ($elapsed,$status,$extra)=$self->call($method,$encoded_params);
+
+        return($elapsed,$status,$extra);
+
+}
+
+sub call($$$){
+        my $self=shift;
+	my $service=shift;
+	my $encoded_params=shift;
+
+        my $t0;
+        my $t1;
+
+	$t0=gettimeofday();
+
+        if ($self->{DEBUG}){
+            $self->trace_log($id,
+                            'call',
+                            scalar(caller),
+                            "ARGS:  method:\"$service\", encoded_params: ".$encoded_params);
+        }
+
+
+        my $response;
+
+        eval {
+                $response = $self->{UA}->get( $self->{URL}."?",$encoded_params);
+                $t1=gettimeofday();
+        } or do {
+                $t1=gettimeofday();
+                return(($t1-$t0),"FAILED_POST",$@);
+        };
+
+        my $elapsed=($t1-$t0);
+
+        if ( ($response->code eq 302 ) || ($response->code eq 301 ) ) {
+            $self->trace_log($id,
+                            'call',
+                            scalar(caller),
+                            "Redirect: ".$response->code);
+            return($elapsed,"REDIRECT","Unexpected Redirect response: ".$response->code);
+        }
+
+        if ( ! ($response->code eq 200 ))  {
+            $self->trace_log($id,
+                            'call',
+                            scalar(caller),
+                            "Bad Response: ".$response->code );
+            return($elapsed,"INVALID_RESPONSE",
+                    "HTTP Code: " . 
+                    $response->code() . 
+                    " Decoded Content: " . 
+                    $response->decoded_content());
+        }
+
+	my $response_content  = $response->decoded_content();
+
+
+        $response_content =~ s/^(?:\357\273\277|\377\376\0\0|\0\0\376\377|\376\377|\377\376)//g;
+
+        my $native_object;
+
+        # <error>Yes</error>
+        if ($respose_content =~ /<error>Yes<\/error>/) {
+            $self->trace_log($id,
+                            'call',
+                            scalar(caller),
+                            "Call Error: ".$response_content);
+            return($elapsed,"CALL_ERROR",$@."\n\n".$response_content);
+        } 
+            $self->trace_log($id,
+                            'call',
+                            scalar(caller),
+                            "Success: ".$response_content);
+        return($elapsed,"SUCCESS",$response_content);
+}
+
+sub trace_log($$$$){
+            my $self=shift;
+            my $id=shift;
+            my $method = shift;
+            my $caller = shift;
+            my $logmsg = shift;
+            #print("$id: Method is \"$method\", caller is \"$caller\", logmsg is \"$logmsg\"\n");
+            print($id . " -- " . caller() ."::".$method." called by \n");
+            print($id . " -- \t" .$caller."\n");
+            print($id . " -- \t\t".$logmsg."\n");
+}
+
+
+1;
