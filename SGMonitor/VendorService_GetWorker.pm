d8c24517eff33ea4c39f3d34af8538672aaa2903
Rick Moran <drhayt@gmail.com>
2015-02-26 11:39:10 -0500

    Added GetWorker

---
+++ SGMonitor/VendorService_GetWorker.pm
@@ -0,0 +1,43 @@
+use strict;
+use warnings;
+package SGMonitor::VendorService_GetWorker;
+use SGMonitor::Helpers::ServiceBus;
+use Data::Dumper;
+use Net::Statsd;
+use Sys::Hostname;
+
+sub new(){
+    my ($class,$args)=@_;
+    my $self = {};
+
+    $self->{SB}=SGMonitor::Helpers::ServiceBus->new( $args );
+    $self->{host}=hostname();
+    $self->{host}=~ s/\./_/g;
+
+    $self->{DEBUG} = $args->{DEBUG} || 0;
+
+    $self->{SERVICE_NAME}="VendorService.GetWorker";
+    $self->{BASE_STRING}="ServiceBus.monitor." . uc($self->{SERVICE_NAME});
+
+    return(bless($self,$class));
+}
+
+sub run(){
+    my $self=shift;
+    my $start=1466;
+    my $range=30;
+    my $propertyid=int(rand($range))+$start;
+
+    my %params=( 'WorkerObjectID' => 1466
+                
+                );
+                #,'IgnoreSPI' => JSON::false );
+
+    my ($elapsed,$status,$extra)=$self->{SB}->call_object($self->{SERVICE_NAME},\%params);
+
+    Net::Statsd::timing($self->{BASE_STRING}.".".$status,$elapsed*1000);
+
+}
+
+
+1;
