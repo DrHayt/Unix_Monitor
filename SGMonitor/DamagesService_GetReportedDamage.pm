68631a639a042b30d5ec961324ef75fb8d10089a
Rick Moran <drhayt@gmail.com>
2015-03-09 08:07:11 -0400

    Added More Damages Workers

---
+++ SGMonitor/DamagesService_GetReportedDamage.pm
@@ -0,0 +1,43 @@
+use strict;
+use warnings;
+package SGMonitor::DamagesService_GetReportedDamage;
+use SGMonitor::Helpers::ServiceBus;
+use Data::Dumper;
+use Net::Statsd;
+use Sys::Hostname;
+
+sub new(){
+    my ($class,$args)=@_;
+    my $self = {};
+
+    $self->{SB}=SGMonitor::Helpers::ServiceBus->new( $args );
+    $self->{host}=hostname();
+    $self->{host}=~ s/\./_/g;
+
+    $self->{DEBUG} = $args->{DEBUG} || 0;
+
+    $self->{SERVICE_NAME}="DamagesService.GetReportedDamage";
+    $self->{BASE_STRING}="ServiceBus.monitor." . uc($self->{SERVICE_NAME});
+
+    return(bless($self,$class));
+}
+
+sub run(){
+    my $self=shift;
+    my $start=1;
+    my $range=3000000;
+    my $number=int(rand($range))+$start;
+
+    # AddValidationRule("DamagesService.GetReportedDamage", "ReportedDamageId", typeof(long));
+
+    my %params=( ReportedDamageId => $number
+                );
+
+    my ($elapsed,$status,$extra)=$self->{SB}->call_object($self->{SERVICE_NAME},\%params);
+
+    Net::Statsd::timing($self->{BASE_STRING}.".".$status,$elapsed*1000);
+
+}
+
+
+1;
