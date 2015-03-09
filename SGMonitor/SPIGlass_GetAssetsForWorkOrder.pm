72a1db3160facfb10042921235ccead9a0789e26
Rick Moran <drhayt@gmail.com>
2015-03-09 13:49:57 -0400

    Added GetAssetsForWorkOrder

---
+++ SGMonitor/SPIGlass_GetAssetsForWorkOrder.pm
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+package SGMonitor::SPIGlass_GetAssetsForWorkOrder;
+use SGMonitor::Helpers::ServiceBus;
+use Data::Dumper;
+use Net::Statsd;
+use Sys::Hostname;
+
+sub new(){
+    my ($class,$args)=@_;
+    my $self = {};
+
+    $self->{SB}=SGMonitor::Helpers::ServiceBus->new( $args );
+    $self->{host}=hostname();
+    $self->{host}=~ s/\./_/g;
+
+    $self->{DEBUG} = $args->{DEBUG} || 0;
+
+    $self->{SERVICE_NAME}="SPIGlass.GetAssetsForWorkOrder";
+    $self->{BASE_STRING}="ServiceBus.monitor." . uc($self->{SERVICE_NAME});
+
+    return(bless($self,$class));
+}
+
+sub run(){
+    my $self=shift;
+    my $start=100100100;
+    my $range= 42000000;
+    my $ordernumber=int(rand($range))+$start;
+
+    my %params=( 'WorkOrderId' => $ordernumber 
+    );
+
+    my ($elapsed,$status,$extra)=$self->{SB}->call_object($self->{SERVICE_NAME},\%params);
+
+    Net::Statsd::timing($self->{BASE_STRING}.".".$status,$elapsed*1000);
+
+}
+
+
+1;
