5d774839647d818e3c67e6398f16b4c15010c7b0
Rick Moran <drhayt@gmail.com>
2015-05-28 07:54:32 -0400

    Added additional Restated

---
+++ SGMonitor/SPIGlass_GetRestatedINSPIBids.pm
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+package SGMonitor::SPIGlass_GetRestatedINSPIBids;
+use SGMonitor::Helpers::ServiceBus;
+use Data::Dumper;
+use Net::Statsd;
+use Sys::Hostname;
+
+sub new(){
+    my ($class,$args)=@_;
+    my $self = {};
+
+    $self->{SB}=SGMonitor::Helpers::ServiceBus->new( $args );
+    $self->{host}=hostname();
+    $self->{host}=~ s/\./_/g;
+
+    $self->{DEBUG} = $args->{DEBUG} || 0;
+
+    $self->{SERVICE_NAME}="SPIGlass.GetRestated";
+    $self->{BASE_STRING}="ServiceBus.monitor." . uc($self->{SERVICE_NAME});
+
+    return(bless($self,$class));
+}
+
+sub run(){
+    my $self=shift;
+    my $start=10010010;
+    my $range=3000000;
+    my $propertyid=int(rand($range))+$start;
+
+    my %params=( 'PropertyId' => $propertyid ,
+                'IncludeBids' => JSON::false,
+                'Source'      => 'INSPI'
+                );
+                #,'IgnoreSPI' => JSON::false );
+
+    my ($elapsed,$status,$extra)=$self->{SB}->call_object($self->{SERVICE_NAME},\%params);
+
+    Net::Statsd::timing($self->{BASE_STRING}.".".$status,$elapsed*1000);
+
+}
+
+
+1;
