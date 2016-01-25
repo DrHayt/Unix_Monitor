6b3167f4dcf560cbd65f606cd3814aa6c2e51589
Rick Moran <drhayt@gmail.com>
2016-01-25 10:07:47 -0500

    Added SPIGlass_GetWorkOrdersOnProperty

---
+++ SGMonitor/SPIGlass_GetWorkOrdersOnProperty.pm
@@ -0,0 +1,38 @@
+use strict;
+use warnings;
+package SGMonitor::SPIGlass_GetWorkOrdersOnProperty;
+use SGMonitor::Helpers::ServiceBus;
+use Data::Dumper;
+use Sys::Hostname;
+
+sub new(){
+    my ($class,$args)=@_;
+    my $self = {};
+
+    $self->{SB}=SGMonitor::Helpers::ServiceBus->new( $args );
+
+    $self->{DEBUG} = $args->{DEBUG} || 0;
+
+    $self->{SERVICE_NAME}="SPIGlass.GetWorkOrdersOnProperty";
+    $self->{BASE_STRING}="ServiceBus.monitor." . uc($self->{SERVICE_NAME});
+
+    return(bless($self,$class));
+}
+
+sub run(){
+    my $self=shift;
+
+    my %params=( ClientCode => 'CMC',
+                 LoanNumber => '1771457996',
+                 SpiPropertyId => 7779074,
+                 IncludeWas => JSON::false,
+                 IncludeCorrectedDamages => JSON::false
+                );
+
+    my ($elapsed,$status,$extra)=$self->{SB}->call_object($self->{SERVICE_NAME},\%params);
+
+    return($self->{BASE_STRING},$elapsed,$status,$extra);
+}
+
+
+1;
